11511e8641321eb5bf6c9fc4de97efa149286e2b
shenxianpeng <xianpeng.shen@gmail.com>
2025-11-17 23:04:00 +0200

    Update setup.py for build

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import sys
 
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
+from setuptools.dist import Distribution
 
 # Add the current directory to the path so we can import gnuplot_wheel
 sys.path.insert(0, os.path.dirname(__file__))
@@ -15,6 +16,15 @@ except ImportError:
     # Fallback if the module isn't available yet
     cmdclass = {"build_py": _build_py}
 
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+
+    def has_ext_modules(self):
+        return True
+
+
 setup(
     cmdclass=cmdclass,
+    distclass=BinaryDistribution,
 )
